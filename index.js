ecf8c3c5a158be81e4ac19f3f242a1a20f2557d8
Your Name <you@example.com>
2022-02-25 20:13:22 +0100

    JS

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-function _id(ident){
+function _id(ident) {
     return document.getElementById(ident);
 }
 
-function factorial(){
-    
+function factorial() {
+    var entrada = _id("entradaFactorial").value;
+    var resultat = 1;
+    var operaciones = "";
+
+    for (var i = 1; i <= entrada; i++) {
+        resultat = resultat * i;
+        operaciones += i + "*"
+    }
+    _id("resultatFactorial").innerHTML = resultat;
+    _id("operacionsFactorial").innerHTML = operaciones;
 }
 
-function calculaIVA(){
-    
-}
+function calculaIVA() {
+    var preuAmbIVA = _id("entradaFactorial").value;
+    var tipusIVA = 1;
+    var preusenseIva = "";
+    var IVA = "";
 
-function canviaFons(tipus){
-    
+    preuAmbIVA = tipusIVA + preuAmbIVA
+    preusenseIva = preuAmbIVA * 0,tipusIVA
+    IVA = 
+
+}
+function canviaFons(tipus) {
+    var colorfondo;
+    var colortexto;
+
+    if (tipus == "clear") {
+        colorfondo = "white";
+        colortexto = "black";
+    } else if (tipus == "random") {
+        var gris = Math.floor(Math.random() * 255);
+        colorfondo = "rgb(" + gris + "," + gris + "," + gris + ")";
+        colortexto = "white";
+    }
+    _id("cos").style.backgroundColor = colorfondo;
+    _id("cos").style.color = colortexto;
 }
